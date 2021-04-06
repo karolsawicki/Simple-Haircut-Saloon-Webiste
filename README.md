df4ff3afa6153744253a7236860058f9c7b7d878
Karol Sawicki <karol.sawicki@gazeta.pl>
2021-04-06 15:23:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# paulinawarachowska-2
+ Pure CSS and HTML Website for haircut saloon. 
---
+++ paulinawarachowska-master/dist/main.js
@@ -0,0 +1,118 @@
+// Animate Smooth Scroll
+$('#view-work').on('click', function() {
+  const NajlepszyFryzjerWPruszkowie = $('#NajlepszyFryzjerWPruszkowie').position().top  - 35;
+
+  $('html, body').animate(
+    {
+      scrollTop: NajlepszyFryzjerWPruszkowie
+    },
+    1400
+  );
+});
+
+window.onload = ZmianaFont;
+//startp = setTimeout(ZmianaFont, 300);
+
+function ZmianaFont(){
+pierwsze = setTimeout(Oregano1, 300);
+drugie = setTimeout(Covered1, 600);
+trzecie = setTimeout(Damion1, 900);
+czwarte = setTimeout(Kaushan1, 1200);
+
+piate = setTimeout(Oregano2, 1500);
+szoste = setTimeout(Covered2, 1800);
+siodme= setTimeout(Damion2, 2100) ;
+osme= setTimeout(Kaushan2, 2400);
+
+dziewiate= setTimeout(Oregano3, 2700);
+dziesiate= setTimeout(Covered3, 3000);
+jedenaste= setTimeout(Damion3, 3300);
+dwunaste = setTimeout(Kaushan3, 3600);
+};
+///////////////////////////////////////////////////////////////////////
+
+function Oregano1(){
+  //Orenago zostaje na zawsze 4
+document.getElementById('h').style.fontFamily = 'Oregano';
+document.getElementById('h').style.fontWeight = "200";
+document.getElementById('h').style.fontSize = "1.5rem";
+};
+
+function Covered1(){
+  // 2
+document.getElementById('h').style.fontFamily = 'Covered By Your Grace';
+document.getElementById('h').style.fontWeight = "200";
+document.getElementById('h').style.fontSize = "1.5rem";
+};
+
+function Damion1(){
+  // 3
+document.getElementById('h').style.fontFamily = 'Damion';
+document.getElementById('h').style.fontWeight = "200";
+document.getElementById('h').style.fontSize = "1.5rem";
+};
+function Kaushan1(){
+  // 3
+document.getElementById('h').style.fontFamily = 'Kaushan Script';
+document.getElementById('h').style.fontWeight = "200";
+document.getElementById('h').style.fontSize = "1.5rem";
+};
+
+///////////////////////////////////////////////////////////////////////
+
+function Oregano2(){
+  //Orenago zostaje na zawsze 4
+document.getElementById('h').style.fontFamily = 'Oregano';
+document.getElementById('h').style.fontWeight = "300";
+document.getElementById('h').style.fontSize = "2.0rem";
+};
+
+function Covered2(){
+  // 2
+document.getElementById('h').style.fontFamily = 'Covered By Your Grace';
+document.getElementById('h').style.fontWeight = "300";
+document.getElementById('h').style.fontSize = "2.0rem";
+};
+
+function Damion2(){
+  // 3
+document.getElementById('h').style.fontFamily = 'Damion';
+document.getElementById('h').style.fontWeight = "300";
+document.getElementById('h').style.fontSize = "2.0rem";
+};
+function Kaushan2(){
+  // 3
+document.getElementById('h').style.fontFamily = 'Kaushan Script';
+document.getElementById('h').style.fontWeight = "300";
+document.getElementById('h').style.fontSize = "2.0rem";
+};
+///////////////////////////////////////////////////////////////////////
+
+function Oregano3(){
+  //Orenago zostaje na zawsze 4
+document.getElementById('h').style.fontFamily = 'Oregano';
+document.getElementById('h').style.fontWeight = "400";
+document.getElementById('h').style.fontSize = "2.5rem";
+};
+
+function Covered3(){
+  // 2
+document.getElementById('h').style.fontFamily = 'Covered By Your Grace';
+document.getElementById('h').style.fontWeight = "400";
+document.getElementById('h').style.fontSize = "2.8rem";
+};
+
+function Damion3(){
+  // 3
+document.getElementById('h').style.fontFamily = 'Damion';
+document.getElementById('h').style.fontWeight = "400";
+document.getElementById('h').style.fontSize = "2.5rem";
+};
+function Kaushan3(){
+  // 3
+document.getElementById('h').style.fontFamily = 'Kaushan Script';
+document.getElementById('h').style.fontWeight = "400";
+document.getElementById('h').style.fontSize = "2.7rem";
+
+};
+///////////////////////////////////////////////////////////////////////
